f121ba34a8be112a908d3b649be454d87870ce36
adelin-diac <105500899+adelin-diac@users.noreply.github.com>
2023-01-26 12:04:15 +0000

    changed robot.ino

--- robot.ino
+++ robot.ino
@@ -4,8 +4,8 @@
 #define leftPin1 39 // Left Wheel pin1 - 2.6
 #define leftPin2 40 // Left Wheel pin2 - 2.7
 
-#define RIGHT_WHEEL_SPEED_MAX 175
-#define LEFT_WHEEL_SPEED_MAX 200
+#define RIGHT_WHEEL_SPEED_MAX 195
+#define LEFT_WHEEL_SPEED_MAX 220
 
 #ifndef __CC3200R1M1RGC__
 // Do not include SPI for CC3200 LaunchPad
@@ -45,23 +45,27 @@ char server[] = "54.78.246.30";
 // that you want to connect to (port 80 is default for HTTP):
 WiFiClient client;
 
-String readResponse();
-String getResponseBody(String& response);
-int getStatusCode(String& response);
-
 int currentPosition = 0;
 int nextPosition;
+
 bool stopConnection = false;
+String serverResponse;
 
-
+int sensorVals[5];
 const int maxSensorVal = 750; // Value for breakpoint of wether sensor is activated
 
 bool moveRobot = true;
-int sensorVals[5];
 
+// Functions declarations
+  // MOVEMENT
 void moveForward(int msDelay, bool slowly);
 void moveBackward(int msDelay);
 void stopRobot();
+  // INTERNET STUFF
+String readResponse();
+String getResponseBody(String& response);
+int getStatusCode(String& response);
+String fetchNextPosition(int currentPos);
 
 void setup() {
     //Initialize serial and wait for port to open:
@@ -91,8 +95,6 @@ void setup() {
   Serial.println("\nIP Address obtained");
   printWifiStatus();
 
-  
-
   pinMode(sensor1, INPUT);
   pinMode(sensor2, INPUT);
   pinMode(sensor3, INPUT);
@@ -105,15 +107,12 @@ void setup() {
   pinMode(leftPin1, OUTPUT);
   pinMode(leftPin2, OUTPUT);
 
-
-
 //  // at start take a reading from the sensors to figure out what is white and what is black
 //  sensorVals[0] = analogRead(sensor1); //left left
 //  sensorVals[1] = analogRead(sensor2); //left
 //  sensorVals[2] = analogRead(sensor3); //middle
 //  sensorVals[3] = analogRead(sensor4); //right
 //  sensorVals[4] = analogRead(sensor5); //right right
-
 }
 
 void loop() {
@@ -129,159 +128,125 @@ void loop() {
 //    Serial.println("stop");
 //    stopRobot();
 //  }
-if(sensorVals[1] < 500 && sensorVals[2] < 500 && sensorVals[3] < 500 || sensorVals[0] < 500 && sensorVals[1] < 500 && sensorVals[2] < 500 || sensorVals[2] < 500 && sensorVals[3] < 500 && sensorVals[4] < 500){
-    stopRobot();
-   
-    Serial.println("\nStarting connection to server...");
-  // if you get a connection, report back via serial:
-  if (client.connect(server, 8081)) {
-    Serial.println("connected to server");
-    // Make a HTTP request:
-    client.println("POST /api/arrived/e6295a5e HTTP/1.1");
-    client.println("Content-Type: application/x-www-form-urlencoded");
-    client.println("Content-Length: 10");
-    client.println();
-    client.println("position=" + String(currentPosition));
-    client.println();
-    
-    String res = readResponse();
-    
 
-      if(getStatusCode(res) == 200){
-        String body = getResponseBody(res);
-        
-        if(body == "undefined"){
-            stopConnection = true;
-        }
-        
-        nextPosition = body.toInt();
-      // Send nextPosition to the other MSP
-      // Other MSP makes robot go to next position
-      // Once there, send a response to this MSP with the current position
-        
-       currentPosition = nextPosition;  // This will be done by communicating to other MSP
-      
-      
+//  if(sensorVals[1] < 500 && sensorVals[2] < 500 && sensorVals[3] < 500 || sensorVals[0] < 500 && sensorVals[1] < 500 && sensorVals[2] < 500 || sensorVals[2] < 500 && sensorVals[3] < 500 && sensorVals[4] < 500){
+
+  // NEED TO FIX TURNING SPEEDS ETC
+  
+  // At junction -> make request if at checkpoint (1)
+  if((sensorVals[1] < maxSensorVal && sensorVals[2] < maxSensorVal && sensorVals[3] < maxSensorVal && sensorVals[0] < maxSensorVal && sensorVals[4] < maxSensorVal)){
+      //check if at checkpoint or just intersection - djisktra algorithm
+      stopRobot();
+//      moveForward(0, true);
+
+      serverResponse = fetchNextPosition(currentPosition);
+
+      if(!stopConnection){
+        nextPosition = serverResponse.toInt();
+      }
+          
+      // compare current position & next position to see route to go on then
+      // set currentPosition
+      delay(1000); // THIS MIMICKS THE MOVEMEMNT OF ROBOT TO NEXT POSITION
       Serial.print("Next Position = ");
       Serial.println(String(nextPosition));
-      Serial.println(res);
-//      Serial.println(body);
-//      Serial.println(nextPosition);
-    delay(1000);
-//    } 
-  }
-  
-  // if there are incoming bytes available
-  // from the server, read them and print them:
-//  while (client.available()) {
-//    char c = client.read();
-//    Serial.write(c);
-//  }
+         
+      currentPosition = nextPosition; // ARrival at next position
+      moveForward(0, false);
+      // If disconnected from server & stopconnection is true -> stop forever at intersection
+      if (!client.connected()) {   
+      // do nothing forevermore:
+        while (stopConnection){
+          stopRobot(); // bring close to wall
+          Serial.println("I have completed my objective");
+          delay(800);
+      } 
+    }
+    return;
+  } 
 
-  client.stop();
-  // if the server's disconnected, stop the client:
-  if (!client.connected()) {
-    Serial.println();
-    Serial.println("disconnecting from server.");
-    
-
-    // do nothing forevermore:
-    while (stopConnection);
-  }
-
-}
-  if(sensorVals[2] < 400 && sensorVals[1] > maxSensorVal && sensorVals[3] > maxSensorVal ){
+  // MOVE FORWARD WHILE MIDDLE ACTIVE AND SENSORS BESIDE NOT ACTIVE (2)
+  if(sensorVals[2] < maxSensorVal && sensorVals[1] > maxSensorVal && sensorVals[3] > maxSensorVal ){
     moveForward(0, false);
     return;
   }
+
+  // MOVE BACK IF GOING OFF TRACK (3)
   if(sensorVals[2] > maxSensorVal && sensorVals[1] > maxSensorVal && sensorVals[3] > maxSensorVal&& sensorVals[0] > maxSensorVal && sensorVals[4] > maxSensorVal){
     stopRobot();
     moveBackward(0);
     return;
   }
+  
+  // TURN RIGHT HERE - SLOW TURN CAUSE MIDDLE STILL ACTIVE (4)
   if(sensorVals[1] < maxSensorVal && sensorVals[2] < maxSensorVal){
-    // NEED TO TURN RIGHT HERE - DECREASE SPEED OF LEFT SLIGHTLY
-    analogWrite(rightPin1, 100);
+    analogWrite(rightPin1, 150);
     analogWrite(rightPin2, 0);
 
-    analogWrite(leftPin1, 75);
+    analogWrite(leftPin1, 120);
     analogWrite(leftPin2, 0);
     return;
   }
+  
+  // TURN LEFT HERE - SLOW TURN CAUSE MIDDLE STILL ACTIVE (5)
   if(sensorVals[3] < maxSensorVal && sensorVals[2] < maxSensorVal){
-    // NEED TO TURN LEFT HERE - DECREASE SPEED OF RIGHT SLIGHTLY
-    analogWrite(leftPin1, 100);
+    analogWrite(leftPin1, 150);
     analogWrite(leftPin2, 0);
     
-    analogWrite(rightPin1, 75);
+    analogWrite(rightPin1, 120);
     analogWrite(rightPin2, 0);
     return;
   }
 
-   if(sensorVals[1] < maxSensorVal){
-    // NEED TO TURN RIGHT HERE - DECREASE SPEED OF LEFT SLIGHTLY
+  // TURN RIGHT - DECREASE SPEED OF LEFT SLIGHTLY (6)
+  if(sensorVals[0] < maxSensorVal && sensorVals[1] < maxSensorVal){
     analogWrite(rightPin1, 100);
     analogWrite(rightPin2, 0);
 
-    analogWrite(leftPin1, 25);
+    analogWrite(leftPin1, 50);
     analogWrite(leftPin2, 0);
-    return;
-  }
-  if(sensorVals[3] < maxSensorVal){
-    // NEED TO TURN LEFT HERE - DECREASE SPEED OF RIGHT SLIGHTLY
-    analogWrite(leftPin1, 100);
-    analogWrite(leftPin2, 0);
-    
-    analogWrite(rightPin1, 25);
-    analogWrite(rightPin2, 0);
     return;
   }
 
-  if(sensorVals[0] < maxSensorVal){ // TURN RIGHT QUICKLY
+  // TURN LEFT - DECREASE SPEED OF RIGHT SLIGHTLY (7)
+  if(sensorVals[3] < maxSensorVal && sensorVals[4] < maxSensorVal){
+    analogWrite(leftPin1, 100);
+    analogWrite(leftPin2, 0);
+    
+    analogWrite(rightPin1, 50);
+    analogWrite(rightPin2, 0);
+    return;
+  }
+  
+  // TURN RIGHT (OR LEFT?) QUICKLY (8)
+  if(sensorVals[0] < maxSensorVal){ 
     analogWrite(leftPin1, 0);
     analogWrite(leftPin2, 100);
     
-    analogWrite(rightPin1, 100);
+    analogWrite(rightPin1, 50);
     analogWrite(rightPin2, 0); 
     return;
   }
-  if(sensorVals[4] < maxSensorVal){ // TURN RIGHT QUICKLY
+  
+  // TURN RIGHT (OR LEFT?) QUICKLY
+  if(sensorVals[4] < maxSensorVal){ 
     analogWrite(rightPin1, 0);
     analogWrite(rightPin2, 100); 
     
-    analogWrite(leftPin1, 100);
+    analogWrite(leftPin1, 50);
     analogWrite(leftPin2, 0);
     return;
   }
-
-  
- 
-  }
-  
-//  else{
-//    moveForward(0);
-//  }
-
-//  Serial.print(sensorVals[0]);
-//  Serial.print("\t");
-//  Serial.print(sensorVals[1]);
-//  Serial.print("\t");
-//  Serial.print(sensorVals[2]);
-//  Serial.print("\t");
-//  Serial.print(sensorVals[3]);
-//  Serial.print("\t");
-//  Serial.print(sensorVals[4]);
-////  Serial.println("");
 }
 
 void moveForward(int msDelay, bool slowly){
   Serial.println("Moving Forward");
   if(slowly){
-    analogWrite(rightPin1, 125);
+    analogWrite(rightPin1, 55);
     analogWrite(rightPin2, 0);
 
     // Left Wheel
-    analogWrite(leftPin1, 105);
+    analogWrite(leftPin1, 55);
     analogWrite(leftPin2, 0);
     return;
   }
@@ -351,7 +316,40 @@ String getResponseBody(String& response){
     return body;
 }
 
+// Get status code of server fetch
 int getStatusCode(String& response){
   String code = response.substring(9,12);
   return code.toInt();
+}
+
+// Make HTTP request to server
+String fetchNextPosition(int currentPos){
+  Serial.println("\nStarting connection to server...");
+  // if you get a connection, report back via serial:
+  if (client.connect(server, 8081)) {
+    Serial.println("connected to server");
+    // Make a HTTP request:
+    client.println("POST /api/arrived/e6295a5e HTTP/1.1");
+    client.println("Content-Type: application/x-www-form-urlencoded");
+    client.println("Content-Length: 10");
+    client.println();
+    client.println("position=" + String(currentPosition));
+    client.println();
+    
+    String res = readResponse();
+    if(getStatusCode(res) == 200){
+        String body = getResponseBody(res);
+        if(body == "undefined"){
+            stopConnection = true;
+        }
+        // stop the client:
+        Serial.println("Stopping Connection");
+        client.stop();
+        return body;
+  }
+  // stop the client:
+  Serial.println("Stopping Connection");
+  client.stop();
+  return "err";
+  }
 }
--- robot_server_connect/robot_server_connect.ino
+++ robot_server_connect/robot_server_connect.ino
@@ -27,6 +27,7 @@ int getStatusCode(String& response);
 int currentPosition = 0;
 int nextPosition;
 bool stopConnection = false;
+String serverResponse;
 
 void setup() {
   //Initialize serial and wait for port to open:
@@ -56,66 +57,33 @@ void setup() {
   Serial.println("\nIP Address obtained");
   printWifiStatus();
 
-  
 }
 
-void loop() {
-//  while(nextPosition < 7){
-    Serial.println("\nStarting connection to server...");
-  // if you get a connection, report back via serial:
-  if (client.connect(server, 8081)) {
-    Serial.println("connected to server");
-    // Make a HTTP request:
-    client.println("POST /api/arrived/e6295a5e HTTP/1.1");
-    client.println("Content-Type: application/x-www-form-urlencoded");
-    client.println("Content-Length: 10");
-    client.println();
-    client.println("position=" + String(currentPosition));
-    client.println();
+void loop() {        
+    String serverResponse = fetchNextPosition(currentPosition);
+    if(!stopConnection){
+      nextPosition = serverResponse.toInt();
+    }
+    // compare current position & next position to see route to go on then
+    // set currentPosition
     
-    String res = readResponse();
-    
-
-      if(getStatusCode(res) == 200){
-        String body = getResponseBody(res);
-        
-        if(body == "undefined"){
-            stopConnection = true;
-        }
-        
-        nextPosition = body.toInt();
-      // Send nextPosition to the other MSP
-      // Other MSP makes robot go to next position
-      // Once there, send a response to this MSP with the current position
-        
-       currentPosition = nextPosition;  // This will be done by communicating to other MSP
-      }
+   currentPosition = nextPosition;  // This will be done by communicating to other MSP
+  }
       
       Serial.print("Next Position = ");
       Serial.println(String(nextPosition));
       Serial.println(res);
 //      Serial.println(body);
 //      Serial.println(nextPosition);
-    delay(1000);
-//    } 
+      delay(1000);
   }
-  
-  // if there are incoming bytes available
-  // from the server, read them and print them:
-//  while (client.available()) {
-//    char c = client.read();
-//    Serial.write(c);
-//  }
+ 
 
-  client.stop();
-  // if the server's disconnected, stop the client:
   if (!client.connected()) {
-    Serial.println();
-    Serial.println("disconnecting from server.");
-    
-
     // do nothing forevermore:
     while (stopConnection);
+    Serial.println("Not connected to server - i will print forever");
+    delay(800);
   }
 }
 
@@ -153,8 +121,35 @@ String getResponseBody(String& response){
     body.trim();
     return body;
 }
-
+// Get status code of server fetch
 int getStatusCode(String& response){
   String code = response.substring(9,12);
   return code.toInt();
+}
+
+// Make HTTP request to server
+String fetchNextPosition(int currentPos){
+  Serial.println("\nStarting connection to server...");
+  // if you get a connection, report back via serial:
+  if (client.connect(server, 8081)) {
+    Serial.println("connected to server");
+    // Make a HTTP request:
+    client.println("POST /api/arrived/e6295a5e HTTP/1.1");
+    client.println("Content-Type: application/x-www-form-urlencoded");
+    client.println("Content-Length: 10");
+    client.println();
+    client.println("position=" + String(currentPosition));
+    client.println();
+    
+    String res = readResponse();
+    if(getStatusCode(res) == 200){
+        String body = getResponseBody(res);
+        if(body == "undefined"){
+            stopConnection = true;
+        }
+        return body;
+  }
+    // stop the client:
+    client.stop();
+  return "err";
 }
